d22b69dc808dee4949a806164f82e829172b8345
Nimzyow <n_soufiani@hotmail.com>
2020-06-07 17:16:08 +0100

    added queries to schema

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,5 +1,41 @@
 const { gql } = require("apollo-server");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+`;
 
 module.exports = typeDefs;
