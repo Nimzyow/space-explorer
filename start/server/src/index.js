1956f8de61b39521e00e83cfb878a0d4d4c7e22f
Nimzyow <n_soufiani@hotmail.com>
2020-06-07 17:05:24 +0100

    created schema and server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,3 +2,10 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 
 const server = new ApolloServer({ typeDefs });
+
+//our app needs to be able to
+// 1) Fetch a list of all upcoming rocket launches
+// 2) Fetch a specific launch by its ID
+// 3) Log in the user
+// 4) Book a launch for a logged-in user
+// 5) Cancel a previously booked launch for a logged-in user
